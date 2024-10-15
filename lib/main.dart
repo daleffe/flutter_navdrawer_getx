049caa3f89e00153bc4b4bb8c18a00a56c7757e8
Guilherme Roberge Daleffe <daleffe@users.noreply.github.com>
2024-10-15 11:14:07 -0300

    Added GetX package and sample drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,196 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(GetMaterialApp(
+    navigatorKey: Get.key,
+    initialRoute: '/home',
+    getPages: [
+      GetPage(
+        name: '/home',
+        page: () => const Home(),
+        binding: HomeBinding(),
+      ),
+      GetPage(
+        name: '/page1',
+        page: () => const Page1(),
+        binding: Page1Binding(),
+      ),
+      GetPage(
+        name: '/page2',
+        page: () => const Page2(),
+        binding: Page2Binding(),
+      ),
+    ],
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainDrawer extends StatelessWidget {
+  const MainDrawer({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Home'),
+            tileColor: Get.currentRoute == '/home' ? Colors.grey[300] : null,
+            onTap: () {
+              print(Get.currentRoute);
+              Get.back();
+              Get.offNamed('/home');
+            },
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            tileColor: Get.currentRoute == '/page1' ? Colors.grey[300] : null,
+            onTap: () {
+              Get.back();
+              Get.offNamed('/page1');
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            tileColor: Get.currentRoute == '/page2' ? Colors.grey[300] : null,
+            onTap: () {
+              Get.back();
+              Get.offNamed('/page2');
+            },
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeBinding extends Bindings {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  void dependencies() {
+    Get.put(HomeController(), permanent: true);
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class Home extends GetView<HomeController> {
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text("APP DRAWER")),
+      body: const Center(
+        child: Text('Home'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      drawer: const MainDrawer(),
     );
+  }
+}
+
+class HomeController extends GetxController {
+  @override
+  void onInit() {
+    print('>>> HomeController init');
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    print('>>> HomeController ready');
+    super.onReady();
+  }
+}
+
+class Page1Binding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(Page1Controller(), permanent: true);
+  }
+}
+
+class Page1 extends GetView<Page1Controller> {
+  const Page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 1')),
+      drawer: const MainDrawer(),
+      body: Center(
+        child: Text(controller.title),
+      ),
+    );
+  }
+}
+
+class Page1Controller extends GetxController {
+  final title = 'Page 1';
+
+  @override
+  void onInit() {
+    print('>>> Page1Controller init');
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    print('>>> Page1Controller ready');
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    print('>>> Page1Controller close');
+    super.onClose();
+  }
+}
+
+class Page2Binding extends Bindings {
+  @override
+  void dependencies() {
+    Get.put(Page2Controller(), permanent: true);
+  }
+}
+
+class Page2 extends GetView<Page2Controller> {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Page 2')),
+      drawer: const MainDrawer(),
+      body: Center(
+        child: Text(controller.title),
+      ),
+    );
+  }
+}
+
+class Page2Controller extends GetxController {
+  final title = 'Page 2';
+
+  @override
+  void onInit() {
+    print('>>> Page2Controller init');
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    print('>>> Page2Controller ready');
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    print('>>> Page2Controller close');
+    super.onClose();
   }
 }
